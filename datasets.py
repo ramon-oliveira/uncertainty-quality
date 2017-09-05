0bad547282460b0a1be45d9228ba68c8e7adcedb
Ramon Oliveira <ramon@roliveira.net>
2017-09-04 23:39:07 -0300

    add mnist dataset

--- datasets.py
+++ datasets.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-
+from keras.datasets import mnist
 
 
 class Dataset(object):
@@ -22,15 +22,15 @@ class Digits(Dataset):
 
         self.classes = np.array(list(set(y)))
         np.random.shuffle(self.classes)
-        self.in_train_classes = self.classes[:4]
-        self.out_train_classes = self.classes[4:8]
-        self.unk_train_classes = self.classes[8:]
+        self.in_train_classes = self.classes[:10]
+        self.out_train_classes = self.classes[10:]
+        self.unk_train_classes = self.classes[10:]
 
-    def train(self, with_unknown=False):
+    def train(self, with_unk=False):
         X = self.X_train[np.in1d(self.y_train, self.in_train_classes)]
         y = self.y_train[np.in1d(self.y_train, self.in_train_classes)]
 
-        if with_unknown:
+        if with_unk:
             X2 = self.X_train[np.in1d(self.y_train, self.unk_train_classes)]
             y2 = self.y_train[np.in1d(self.y_train, self.unk_train_classes)]
 
@@ -39,9 +39,79 @@ class Digits(Dataset):
 
         return X, y
 
-    def test(self):
-        X = self.X_test[~np.in1d(self.y_test, self.unk_train_classes)]
-        y = self.y_test[~np.in1d(self.y_test, self.unk_train_classes)]
+    def test(self, with_out=False, with_unk=False):
+        X = self.X_test[np.in1d(self.y_test, self.in_train_classes)]
+        y = self.y_test[np.in1d(self.y_test, self.in_train_classes)]
+
+        if with_out:
+            X2 = self.X_test[np.in1d(self.y_test, self.out_train_classes)]
+            y2 = self.y_test[np.in1d(self.y_test, self.out_train_classes)]
+
+            X = np.concatenate((X, X2), axis=0)
+            y = np.concatenate((y, X2), axis=0)
+
+        if with_unk:
+            X2 = self.X_test[np.in1d(self.y_test, self.unk_train_classes)]
+            y2 = self.y_test[np.in1d(self.y_test, self.unk_train_classes)]
+
+            X = np.concatenate((X, X2), axis=0)
+            y = np.concatenate((y, X2), axis=0)
+
+        return X, y
+
+
+
+class MNIST(Dataset):
+
+    def __init__(self, test_size, *args, **kwargs):
+        super(MNIST, self).__init__(*args, **kwargs)
+
+        (X_train, y_train), (X_test, y_test) = mnist.load_data()
+        print(X_train.max(), X_train.min())
+        X_train = X_train.reshape(X_train.shape[0], -1)/128
+        X_test = X_test.reshape(X_test.shape[0], -1)/128
+
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
+
+        self.classes = np.array(list(set(y_train)))
+        np.random.shuffle(self.classes)
+        self.in_train_classes = self.classes[:10]
+        self.out_train_classes = self.classes[10:]
+        self.unk_train_classes = self.classes[10:]
+
+    def train(self, with_unk=False):
+        X = self.X_train[np.in1d(self.y_train, self.in_train_classes)]
+        y = self.y_train[np.in1d(self.y_train, self.in_train_classes)]
+
+        if with_unk:
+            X2 = self.X_train[np.in1d(self.y_train, self.unk_train_classes)]
+            y2 = self.y_train[np.in1d(self.y_train, self.unk_train_classes)]
+
+            X = np.concatenate((X, X2), axis=0)
+            y = np.concatenate((y, X2), axis=0)
+
+        return X, y
+
+    def test(self, with_out=False, with_unk=False):
+        X = self.X_test[np.in1d(self.y_test, self.in_train_classes)]
+        y = self.y_test[np.in1d(self.y_test, self.in_train_classes)]
+
+        if with_out:
+            X2 = self.X_test[np.in1d(self.y_test, self.out_train_classes)]
+            y2 = self.y_test[np.in1d(self.y_test, self.out_train_classes)]
+
+            X = np.concatenate((X, X2), axis=0)
+            y = np.concatenate((y, X2), axis=0)
+
+        if with_unk:
+            X2 = self.X_test[np.in1d(self.y_test, self.unk_train_classes)]
+            y2 = self.y_test[np.in1d(self.y_test, self.unk_train_classes)]
+
+            X = np.concatenate((X, X2), axis=0)
+            y = np.concatenate((y, X2), axis=0)
 
         return X, y
 
@@ -51,6 +121,8 @@ def load(dataset):
 
     if name == 'digits':
         dataset = Digits(**dataset)
+    elif name == 'mnist':
+        dataset = MNIST(**dataset)
     else:
         raise Exception('Unknown dataset {0}'.format(dataset))
 
