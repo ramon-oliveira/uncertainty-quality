680ed04d45b4aec044d8d1dc1d4d2093f442092d
Ramon Oliveira <ramon@roliveira.net>
2017-09-18 00:12:39 -0300

    add run for protein

---
+++ runs/_sources/datasets_bd0719ff3a9a3566954bc316180f6cce.py
@@ -0,0 +1,250 @@
+import tqdm
+import numpy as np
+import pandas as pd
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from keras.datasets import mnist
+from keras.datasets import cifar10
+from keras.datasets import boston_housing
+from keras.preprocessing.image import ImageDataGenerator
+import keras.backend as K
+import re
+
+
+class Dataset(object):
+
+    def __init__(self, type):
+        self.type = type
+
+
+class MNIST(Dataset):
+
+    def __init__(self, *args, **kwargs):
+        super(MNIST, self).__init__('classification', *args, **kwargs)
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+        if K.image_data_format() == 'channels_first':
+            x_train = x_train.reshape(-1, 1, 28, 28)/255
+            x_test = x_test.reshape(-1, 1, 28, 28)/255
+        else:
+            x_train = x_train.reshape(-1, 28, 28, 1)/255
+            x_test = x_test.reshape(-1, 28, 28, 1)/255
+
+        y_train = np.eye(10)[y_train.ravel()]
+        y_test = np.eye(10)[y_test.ravel()]
+
+        self.input_shape = x_train.shape[1:]
+        self.output_size = 10
+
+        split = 50000
+        self.x_train = x_train[:split]
+        self.y_train = y_train[:split]
+        self.x_val = x_train[split:]
+        self.y_val = y_train[split:]
+        self.x_test = x_test
+        self.y_test = y_test
+
+
+class CIFAR10(Dataset):
+
+    def __init__(self, *args, **kwargs):
+        super(CIFAR10, self).__init__('classification', *args, **kwargs)
+        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+        y_train = np.eye(10)[y_train.ravel()]
+        y_test = np.eye(10)[y_test.ravel()]
+
+        self.input_shape = x_train.shape[1:]
+        self.output_size = 10
+
+        split = 42000
+        self.x_train = x_train[:split]
+        self.y_train = y_train[:split]
+        self.x_val = x_train[split:]
+        self.y_val = y_train[split:]
+        self.x_test = x_test
+        self.y_test = y_test
+
+
+class Melanoma(Dataset):
+
+    def __init__(self, *args, **kwargs):
+        super(Melanoma, self).__init__('classification', *args, **kwargs)
+
+        x_train = np.load('data/melanoma/x_train.npy').astype('float32')
+        y_train = np.load('data/melanoma/y_train.npy').astype('int32')
+        x_test = np.load('data/melanoma/x_test.npy').astype('float32')
+        y_test = np.load('data/melanoma/y_test.npy').astype('int32')
+
+        y_train = np.eye(2)[y_train.ravel()]
+        y_test = np.eye(2)[y_test.ravel()]
+
+        if K.image_data_format() == 'channels_first':
+            x_train = x_train.reshape(-1, 3, 224, 224)/255
+            x_test = x_test.reshape(-1, 3, 224, 224)/255
+        else:
+            x_train = x_train.reshape(-1, 224, 224, 3)/255
+            x_test = x_test.reshape(-1, 224, 224, 3)/255
+
+        self.input_shape = x_train.shape[1:]
+        self.output_size = 2
+
+        datagen = ImageDataGenerator(
+            rotation_range=30,
+            width_shift_range=0.2,
+            height_shift_range=0.2,
+            shear_range=0.1,
+            zoom_range=0.1,
+            fill_mode='nearest',
+            horizontal_flip=True,
+            vertical_flip=True,
+        )
+
+        n = 48000
+        xs = []
+        ys = []
+        pbar = tqdm.tqdm(total=n)
+        for x, y in datagen.flow(x_train, y_train, batch_size=1000):
+            xs.append(x)
+            ys.append(y)
+            pbar.update(1000)
+            if len(xs)*1000 == n:
+                pbar.close()
+                break
+        xs.append(x_train)
+        ys.append(y_train)
+
+        x_train = np.vstack(xs)
+        y_train = np.vstack(ys)
+
+        split = 42000
+        self.x_train = x_train[:split]
+        self.y_train = y_train[:split]
+        self.x_val = x_train[split:]
+        self.y_val = y_train[split:]
+        self.x_test = x_test
+        self.y_test = y_test
+
+
+class BostonHousing(Dataset):
+
+    def __init__(self, *args, **kwargs):
+        super(BostonHousing, self).__init__('regression', *args, **kwargs)
+        (x_train, y_train), (x_test, y_test) = boston_housing.load_data()
+        y_train = np.hstack([y_train.reshape(-1, 1), -np.ones(shape=[len(y_train), 1])])
+        y_test = np.hstack([y_test.reshape(-1, 1), -np.ones(shape=[len(y_test), 1])])
+
+        xscaler = StandardScaler().fit(x_train)
+        x_train = xscaler.transform(x_train)
+        x_test = xscaler.transform(x_test)
+        self.xscaler = xscaler
+
+        yscaler = StandardScaler().fit(y_train)
+        y_train = yscaler.transform(y_train)
+        y_test = yscaler.transform(y_test)
+        self.yscaler = yscaler
+
+        self.input_shape = x_train.shape[1:]
+        self.output_size = 2
+
+        split = int(len(x_train)*0.8)
+        self.x_train = x_train[:split]
+        self.y_train = y_train[:split]
+        self.x_val = x_train[split:]
+        self.y_val = y_train[split:]
+        self.x_test = x_test
+        self.y_test = y_test
+
+        print(self.x_train.shape, self.y_train.shape)
+        print(self.x_val.shape, self.y_val.shape)
+        print(self.x_test.shape, self.y_test.shape)
+
+
+class Kin8nm(Dataset):
+
+    def __init__(self, *args, **kwargs):
+        super(Kin8nm, self).__init__('regression', *args, **kwargs)
+        df = pd.read_csv('data/kin8nm/kin8nm.csv', header=None)
+        df[9] = -1.0
+        x = df.values[:, :8]
+        y = df.values[:, 8:]
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+        xscaler = StandardScaler().fit(x_train)
+        x_train = xscaler.transform(x_train)
+        x_test = xscaler.transform(x_test)
+        self.xscaler = xscaler
+
+        yscaler = StandardScaler().fit(y_train)
+        y_train = yscaler.transform(y_train)
+        y_test = yscaler.transform(y_test)
+        self.yscaler = yscaler
+
+        self.input_shape = x_train.shape[1:]
+        self.output_size = 2
+
+        split = int(len(x_train)*0.8)
+        self.x_train = x_train[:split]
+        self.y_train = y_train[:split]
+        self.x_val = x_train[split:]
+        self.y_val = y_train[split:]
+        self.x_test = x_test
+        self.y_test = y_test
+
+
+class ProteinStructure(Dataset):
+
+    def __init__(self, *args, **kwargs):
+        super(ProteinStructure, self).__init__('regression', *args, **kwargs)
+        df = pd.read_csv('data/protein_structure/protein_structure.csv')
+        x = df.values[:, 1:].astype('float32')
+        y = df.values[:, 0:1].astype('float32')
+        y = np.hstack([y, -np.ones(shape=[len(y), 1])])
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
+
+        xscaler = StandardScaler().fit(x_train)
+        x_train = xscaler.transform(x_train)
+        x_test = xscaler.transform(x_test)
+        self.xscaler = xscaler
+
+        yscaler = StandardScaler().fit(y_train)
+        y_train = yscaler.transform(y_train)
+        y_test = yscaler.transform(y_test)
+        self.yscaler = yscaler
+
+        self.input_shape = x_train.shape[1:]
+        self.output_size = 2
+
+        split = int(len(x_train)*0.8)
+        self.x_train = x_train[:split]
+        self.y_train = y_train[:split]
+        self.x_val = x_train[split:]
+        self.y_val = y_train[split:]
+        self.x_test = x_test
+        self.y_test = y_test
+
+
+def load(settings):
+    name = settings.pop('name')
+
+    if name == 'digits':
+        dataset = Digits(**settings)
+    elif name == 'mnist':
+        dataset = MNIST(**settings)
+    elif name == 'cifar10':
+        dataset = CIFAR10(**settings)
+    elif name == 'melanoma':
+        dataset = Melanoma(**settings)
+    elif name == 'boston_housing':
+        dataset = BostonHousing(**settings)
+    elif name == 'kin8nm':
+        dataset = Kin8nm(**settings)
+    elif name == 'protein_structure':
+        dataset = ProteinStructure(**settings)
+    else:
+        raise Exception('Unknown dataset {0}'.format(name))
+
+    settings['name'] = name
+
+    return dataset
---
+++ runs/_sources/evaluate_regression_6749d9de82c88fbb2169c329ca81b5b5.py
@@ -0,0 +1,158 @@
+import numpy as np
+import xgboost as xgb
+from scipy.stats import entropy
+from sklearn import linear_model
+from scipy.stats import norm
+from scipy.misc import logsumexp
+
+
+def uncertainty_std_predicted_mean(y_probabilistic):
+    return y_probabilistic.std(axis=0)[:, 0]
+
+
+def uncertainty_predicted_std(y_probabilistic):
+    return y_probabilistic.mean(axis=0)[:, 1]
+
+
+def uncertainty_predicted_std_det(y_deterministic):
+    return y_deterministic[:, 1]
+
+
+def uncertainty_std_predicted_std(y_probabilistic):
+    return y_probabilistic.std(axis=0)[:, 1]
+
+
+def uncertainty_nll_gaussian(y_det_prob):
+    y_deterministic, y_probabilistic = y_det_prob
+    def log_gaussian2(x, mean, log_std):
+        log_var = 2*log_std
+        return -np.log(2*np.pi)/2.0 - log_var/2.0 - (x-mean)**2/(2*np.exp(log_var))
+
+    uncertainty = []
+    for i, (mean, log_std) in enumerate(y_deterministic):
+        u = np.mean(log_gaussian2(y_probabilistic[:, 0], mean, log_std))
+        uncertainty.append(u)
+    return np.array(uncertainty)
+
+
+def uncertainty_nll_gaussian_std(y_det_prob):
+    y_deterministic, y_probabilistic = y_det_prob
+    def log_gaussian2(x, mean, log_std):
+        log_var = 2*log_std
+        return -np.log(2*np.pi)/2.0 - log_var/2.0 - (x-mean)**2/(2*np.exp(log_var))
+
+    uncertainty = []
+    for i, (mean, log_std) in enumerate(y_deterministic):
+        u = np.std(log_gaussian2(y_probabilistic[:, 0], mean, log_std))
+        uncertainty.append(u)
+    return np.array(uncertainty)
+
+
+def uncertainty_classifer(model, dataset, test_uncertainty):
+    x_val = np.vstack([dataset.x_train, dataset.x_val])
+    y_val = np.vstack([dataset.y_train, dataset.y_val])
+    # x_val, y_val = dataset.x_val, dataset.y_val
+
+    y_deterministic = model.predict(x_val, probabilistic=False)
+    y_probabilistic = model.predict(x_val, probabilistic=True)
+
+    uncertainties = [
+        ('uncertainty_std_predicted_mean', uncertainty_std_predicted_mean, (y_probabilistic)),
+        ('uncertainty_predicted_std', uncertainty_predicted_std, (y_probabilistic)),
+        ('uncertainty_predicted_std_det', uncertainty_predicted_std_det, (y_deterministic)),
+        ('uncertainty_std_predicted_std', uncertainty_std_predicted_std, (y_probabilistic)),
+        ('uncertainty_nll_gaussian', uncertainty_nll_gaussian, (y_deterministic, y_probabilistic)),
+        ('uncertainty_nll_gaussian_std', uncertainty_nll_gaussian_std, (y_deterministic, y_probabilistic)),
+    ]
+
+    x = np.zeros([len(y_val), len(uncertainties) + dataset.output_size])
+    x[:, len(uncertainties):] = y_probabilistic.mean(axis=0)
+    # use as target
+    y = (y_val[:, 0] - y_probabilistic.mean(axis=0)[:, 0])**2
+
+    for i, (name, func, y_score) in enumerate(uncertainties):
+        uncertainty = func(y_score)
+        x[:, i] = uncertainty
+
+    rg = xgb.XGBRegressor(max_depth=2).fit(x, y)
+    # rg = linear_model.RidgeCV(alphas=np.linspace(0.1, 10, 20)).fit(x, y)
+    uncertainty = rg.predict(test_uncertainty)
+    return uncertainty
+
+
+def evaluate(model, dataset):
+    info = {}
+    x_test, y_test = dataset.x_test, dataset.y_test
+
+    y_deterministic = model.predict(x_test, probabilistic=False)
+    y_probabilistic = model.predict(x_test, probabilistic=True)
+
+    y_true = dataset.yscaler.inverse_transform(y_test)
+    y_pred = dataset.yscaler.inverse_transform(y_probabilistic.mean(axis=0))
+
+    rmse_test = np.sqrt(np.mean((y_true[:, 0] - y_pred[:, 0])**2))
+    info['rmse_test'] = rmse_test
+    print('rmse test: {0:.4f}'.format(rmse_test))
+
+    uncertainties = [
+        ('uncertainty_std_predicted_mean', uncertainty_std_predicted_mean, (y_probabilistic)),
+        ('uncertainty_predicted_std', uncertainty_predicted_std, (y_probabilistic)),
+        ('uncertainty_predicted_std_det', uncertainty_predicted_std_det, (y_deterministic)),
+        ('uncertainty_std_predicted_std', uncertainty_std_predicted_std, (y_probabilistic)),
+        ('uncertainty_nll_gaussian', uncertainty_nll_gaussian, (y_deterministic, y_probabilistic)),
+        ('uncertainty_nll_gaussian_std', uncertainty_nll_gaussian_std, (y_deterministic, y_probabilistic)),
+    ]
+
+    test_uncertainty = np.zeros([len(y_test), len(uncertainties) + dataset.output_size])
+    test_uncertainty[:, len(uncertainties):] = y_probabilistic.mean(axis=0)
+    for i, (name, func, y_score) in enumerate(uncertainties):
+        uncertainty = func(y_score)
+        l = np.array(sorted(zip(uncertainty, y_true[:, 0], y_pred[:, 0])))
+        prop_acc = []
+        for end in range(100, len(y_pred), 20):
+            prop = end/len(y_pred)
+            rmse = np.sqrt(np.mean((l[:end, 1].squeeze() - l[:end, 2].squeeze())**2))
+            prop_acc.append([prop, rmse])
+
+        auc = np.trapz(y=np.array(prop_acc)[:, 1], x=np.array(prop_acc)[:, 0])
+        print(name, auc)
+        info[name] = prop_acc
+        info[name+'_auc'] = auc
+        test_uncertainty[:, i] = uncertainty
+
+    uncertainty = uncertainty_classifer(model, dataset, test_uncertainty)
+    l = np.array(sorted(zip(uncertainty, y_true[:, 0], y_pred[:, 0])))
+    prop_acc = []
+    for end in range(100, len(y_pred), 20):
+        prop = end/len(y_pred)
+        rmse = np.sqrt(np.mean((l[:end, 1].squeeze() - l[:end, 2].squeeze())**2))
+        prop_acc.append([prop, rmse])
+    auc = np.trapz(y=np.array(prop_acc)[:, 1], x=np.array(prop_acc)[:, 0])
+    print('uncertainty_classifer', auc)
+    info['uncertainty_classifer'] = prop_acc
+    info['uncertainty_classifer_auc'] = auc
+
+    ll = []
+    for x, mean, sigma2 in zip(y_pred[:, 0], y_true[:, 0], uncertainty):
+        sigma2 = max(1e-6, sigma2)
+        sigma = np.sqrt(sigma2)
+        ll.append(norm.logpdf(x, loc=mean, scale=sigma))
+    ll = np.array(ll)
+    ll_test = np.mean(ll)
+    print('log-likelihood normal:', ll_test)
+    info['ll_normal'] = ll_test
+
+    tau = 0.159707652696 # obtained from BO
+    ll = (logsumexp(-0.5 * tau * (y_true[:, 0] - y_pred[:, 0])**2, axis=0) - np.log(model.posterior_samples)
+          - 0.5*np.log(2*np.pi) - 0.5*np.log(tau))
+    print('log-likelihood tau:', np.mean(ll))
+    info['ll_tau'] = ll_test
+
+    tau = uncertainty
+    tau[tau <= 0] = 1e-6
+    ll = (logsumexp(-0.5 * tau * (y_true[:, 0] - y_pred[:, 0])**2, axis=0) - np.log(model.posterior_samples)
+          - 0.5*np.log(2*np.pi) - 0.5*np.log(tau))
+    print('log-likelihood uncertainty:', np.mean(ll))
+    info['ll_uncertainty'] = ll_test
+
+    return info
